8ce5dbc2ca298e0916107509c28e1d1df7365d4a
demien <demien92@yandex.ru>
2019-05-13 19:07:19 +0300

    commit

--- Lesson_2/index.js
+++ Lesson_2/index.js
@@ -1,1 +1,39 @@
+/// 1 ///
 
+function removeKeys(a, b) {
+    var obj = {};
+    for (var el in a) {
+        var arr = b.filter(function (key ){
+            return key === el;
+        });
+        if(arr.length === 0) {
+            obj[el] = a[el];
+        }
+    }
+    return obj;
+}
+// console.log(removeKeys({ a: 1, b: [], c: '' }, ['a', 'c']));
+
+/// 2 ///
+
+// var ar = ([[1,"a",()=>{}],[{}, 2, 3],[null, -3, 'q']]);
+
+var arr2 = ar.map(function ( num){
+    return num.filter(function(number) {
+        return typeof number === 'number'
+    });
+
+});
+
+/// 3 ///
+
+// var result = reverse('123', '456');
+
+function reverse() {
+    var args = Array.prototype.slice.call(arguments, reverse.length);
+    return args.map(function(el) {
+        return el.split('').reverse().join('');
+    }).reverse();
+}
+
+/// 4 ///
