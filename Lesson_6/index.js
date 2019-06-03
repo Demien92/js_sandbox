4a2141bc30f0771fcb67064bbd7810fbf41099ab
demien <demien92@yandex.ru>
2019-06-03 21:16:24 +0300

    commit

--- Lesson_6/index.js
+++ Lesson_6/index.js
@@ -1,9 +1,65 @@
-//const camelCase = require('camelcase');
+import randomEmail from 'random-email';
+import emailValidator from 'email-validator';
+import formatValidator from 'format-validator';
 
-import camelCase from 'camelcase';
+const generate = document.getElementById('generate');
+const popup = document.getElementById('popup');
+const popupEmail = document.getElementById('popupEmail');
+const close = document.getElementById('close');
 
-console.log(camelCase('foo-bar'));
+generate.addEventListener("click", (event) => {
+    popup.style.display = "block";
+    popupEmail.innerHTML = `${randomEmail({domain: 'test.ua'})}`;
+});
+close.addEventListener("click", (event) => {
+    generate.style.display = "block";
+    popup.style.display = "none";
+});
 
-document.querySelector('div');
+const input = document.getElementById('input');
 
-div.addEventListener('change', );
+input.addEventListener("change", (event) => {
+    if(input.value === ""){
+        input.style.border = "2px solid grey";
+    }else if(emailValidator.validate(input.value) === true){
+        input.style.border = "2px solid green";
+    } else if(emailValidator.validate(input.value) === false){
+        input.style.border = "2px solid red";
+    }
+});
+
+
+//Задание 3
+const check = document.getElementById('check');
+const popupCheck = document.getElementById('popupCheck');
+const popupTypes = document.getElementById('popupTypes');
+const closeCheck = document.getElementById('closeCheck');
+const inputFormat = document.getElementById('inputFormat');
+let format = [];
+let str = "";
+
+for (let key in formatValidator) {
+    if (key[0] === "i" && key[1] === "s"){
+        format.push(key);
+    }
+}
+check.addEventListener("click", (event) => {
+    for (let i = 0; i < format.length; i += 1){
+        if (format[i] !== "isDate" && format[i] !== "isTrashEmail" && format[i] !== "isValid"){
+            if(formatValidator[format[i]](inputFormat.value)){
+                for (let j = 2; j < format[i].length; j += 1){
+                    str += format[i].charAt(j);
+                }
+                str += ", "
+            }
+        }
+    }
+    str = str.slice(0, str.length - 2);
+    popupCheck.style.display = "block";
+    popupTypes.innerHTML = str;
+});
+closeCheck.addEventListener("click", (event) => {
+    check.style.display = "block";
+    popupCheck.style.display = "none";
+    str = "";
+});
